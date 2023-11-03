5de2a1dc2c017deed52356eb461a6682a63bb524
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-03 13:21:00 +0530

    ver 4

--- app/src/main/java/com/book/auto/driver/App.kt
+++ app/src/main/java/com/book/auto/driver/App.kt
@@ -1,26 +1,9 @@
 package com.book.auto.driver
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.graphics.Color
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        val channel = NotificationChannel(
-            "location",
-            "Location",
-            NotificationManager.IMPORTANCE_HIGH
-        ).apply {
-            lightColor = Color.BLUE
-            enableLights(true)
-        }
-    }
-
 
 }
