47366415c5e4dc2a68c4add0257e38bcf1b4051f
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-03 13:59:16 +0530

    ver 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.book.auto.driver"
         minSdk = 28
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
