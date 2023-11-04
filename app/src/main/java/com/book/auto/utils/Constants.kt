79117a04497b792f7f14858981ed1a25a74fd5f7
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-05 00:00:55 +0530

    v 5

--- app/src/main/java/com/book/auto/utils/Constants.kt
+++ app/src/main/java/com/book/auto/utils/Constants.kt
@@ -12,7 +12,7 @@ class Constants {
             "https://play.google.com/store/apps/details?id=com.book.auto.driver"
         const val APK_SHARE_MSG =
             "Hey!! I am using Book Auto app for search nearby vehicles, you can also try., link:- $PLAYSTORE_URL Thanks!!"
-        val autoTypes = listOf("Car", "Auto", "Bike", "Other")
+        val autoTypes = listOf("4 Wheeler", "3 Wheeler", "2 Wheeler", "Other")
     }
 }
 
