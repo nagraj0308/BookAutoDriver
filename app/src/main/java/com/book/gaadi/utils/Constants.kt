49bc5861fec47231fa9f817e71ce03773468d377
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-11 07:37:22 +0530

    muli

--- app/src/main/java/com/book/gaadi/utils/Constants.kt
+++ app/src/main/java/com/book/gaadi/utils/Constants.kt
@@ -8,7 +8,7 @@ class Constants {
         const val PNP_URL = "https://nagraj0308.github.io/book_gaadi_pnp/"
         const val PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.book.gaadi"
         const val APK_SHARE_MSG =
-            "Hey!! I am using Book Gaadi app for search nearby vehicles, you can also try., link:- $PLAYSTORE_URL Thanks!!"
+            "Book Gaadi, Nearby vehicle searching app for booking purpose, install from $PLAYSTORE_URL "
 
         val autoTypes = listOf("Car", "Auto", "Bike", "Bus")
     }
