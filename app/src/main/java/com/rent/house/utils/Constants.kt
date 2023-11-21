d44c30232bfa549689ed1a10f6b9f53251cdc6e0
nagraj0308 <nagendrachaudhary2016@gmail.com>
2023-11-21 15:47:29 +0530

    v1

--- app/src/main/java/com/rent/house/utils/Constants.kt
+++ app/src/main/java/com/rent/house/utils/Constants.kt
@@ -8,7 +8,7 @@ class Constants {
         const val PNP_URL = "https://nagraj0308.github.io/rent_house_pnp/"
         const val PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.rent.house"
         const val APK_SHARE_MSG =
-            "किराय पर हो मकान लेना या अपने मकान को किराए पर देना, डाउनलोड करें Rent House ऐप । $PLAYSTORE_URL "
+            "अपने आस-पास किराय पर हो मकान लेना या अपने मकान को किराए पर देना, डाउनलोड करें Rent House ऐप । $PLAYSTORE_URL "
 
     }
 }
