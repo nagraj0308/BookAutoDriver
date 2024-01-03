f91f69cb28ad76077439e3417a1b8dbeb320fd7a
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-01-03 23:33:38 +0530

    ghgh

--- app/src/main/java/com/rent/house/utils/Constants.kt
+++ app/src/main/java/com/rent/house/utils/Constants.kt
@@ -3,8 +3,8 @@ package com.rent.house.utils
 class Constants {
     companion object {
         const val BASE_URL = "https://server0308.azurewebsites.net/"
-        const val ABOUT_US_URL = "https://nagraj0308.github.io/rent_house_aboutus/"
-        const val PNP_URL = "https://nagraj0308.github.io/rent_house_pnp/"
-        const val PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.rent.house"
+        const val ABOUT_US_URL = "https://nagraj0308.github.io/book_homestay_aboutus/"
+        const val PNP_URL = "https://nagraj0308.github.io/book_homestay_pnp/"
+        const val PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.book.homestay"
     }
 }
