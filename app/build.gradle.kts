dfa3e9e958706867c8240dfd2aed2d373e69869e
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-01-04 23:55:32 +0530

    ghgh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.book.homestay"
         minSdk = 27
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.7"
+        versionCode = 1
+        versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
