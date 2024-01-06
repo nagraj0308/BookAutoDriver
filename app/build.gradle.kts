8b2d5a7ebd8c369c41fed5746faacec174004de4
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-01-06 10:02:09 +0530

    ghgh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.rent.house"
         minSdk = 27
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.7"
+        versionCode = 8
+        versionName = "0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
