d54c4ebd140a5a4332cc929c7c599897f826bb16
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-05-16 23:22:07 +0530

    fc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.book.auto"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
