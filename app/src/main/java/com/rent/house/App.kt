08a7768c17cd0362a1e170a50ed5adc59baa2ada
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-07-14 08:19:11 +0530

    fc

--- app/src/main/java/com/rent/house/App.kt
+++ app/src/main/java/com/rent/house/App.kt
@@ -6,7 +6,13 @@ import android.app.Application
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
@@ -15,12 +21,13 @@ import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.Arrays
+import java.util.Date
 
 @HiltAndroidApp
-class App : Application(), Application.ActivityLifecycleCallbacks {
+class App : Application(), Application.ActivityLifecycleCallbacks, LifecycleObserver {
     private lateinit var appOpenAdManager: AppOpenAdManager
     private var currentActivity: Activity? = null
+
 
     override fun onCreate() {
         super.onCreate()
@@ -30,11 +37,22 @@ class App : Application(), Application.ActivityLifecycleCallbacks {
             // Initialize the Google Mobile Ads SDK on a background thread.
             MobileAds.initialize(this@App) {}
         }
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
         appOpenAdManager = AppOpenAdManager()
 
         val testDeviceIds = listOf("6C438043FBAE987EE0BA45F40294826C")
         val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
         MobileAds.setRequestConfiguration(configuration)
+    }
+
+
+    /** LifecycleObserver method that shows the app open ad when the app moves to foreground. */
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onMoveToForeground() {
+        // Show the ad (if available) when the app moves to foreground.
+        currentActivity?.let {
+            appOpenAdManager.showAdIfAvailable(it)
+        }
     }
 
     override fun onActivityStarted(activity: Activity) {
@@ -50,6 +68,10 @@ class App : Application(), Application.ActivityLifecycleCallbacks {
         private var appOpenAd: AppOpenAd? = null
         private var isLoadingAd = false
         var isShowingAd = false
+
+        /** Keep track of the time an app open ad is loaded to ensure you don't show an expired ad. */
+        private var loadTime: Long = 0
+
 
         /** Request an ad. */
         fun loadAd(context: Context) {
@@ -70,6 +92,7 @@ class App : Application(), Application.ActivityLifecycleCallbacks {
                         Log.d("NAGRAJ", "Ad was loaded.")
                         appOpenAd = ad
                         isLoadingAd = false
+                        loadTime = Date().time
                     }
 
                     override fun onAdFailedToLoad(loadAdError: LoadAdError) {
@@ -80,9 +103,78 @@ class App : Application(), Application.ActivityLifecycleCallbacks {
                 })
         }
 
-        /** Check if ad exists and can be shown. */
+
+        fun showAdIfAvailable(activity: Activity) {
+            showAdIfAvailable(
+                activity,
+                object : OnShowAdCompleteListener {
+                    override fun onShowAdComplete() {
+                        // Empty because the user will go back to the activity that shows the ad.
+                    }
+                })
+        }
+
+        /** Shows the ad if one isn't already showing. */
+        fun showAdIfAvailable(
+            activity: Activity,
+            onShowAdCompleteListener: OnShowAdCompleteListener
+        ) {
+            // If the app open ad is already showing, do not show the ad again.
+            if (isShowingAd) {
+                Log.d("NAGRAJ", "The app open ad is already showing.")
+                return
+            }
+
+            // If the app open ad is not available yet, invoke the callback then load the ad.
+            if (!isAdAvailable()) {
+                Log.d("NAGRAJ", "The app open ad is not ready yet.")
+                onShowAdCompleteListener.onShowAdComplete()
+                loadAd(activity)
+                return
+            }
+
+            appOpenAd?.setFullScreenContentCallback(
+                object : FullScreenContentCallback() {
+
+                    override fun onAdDismissedFullScreenContent() {
+                        // Called when full screen content is dismissed.
+                        // Set the reference to null so isAdAvailable() returns false.
+                        Log.d("NAGRAJ", "Ad dismissed fullscreen content.")
+                        appOpenAd = null
+                        isShowingAd = false
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        loadAd(activity)
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        // Called when fullscreen content failed to show.
+                        // Set the reference to null so isAdAvailable() returns false.
+                        Log.d("NAGRAJ", adError.message)
+                        appOpenAd = null
+                        isShowingAd = false
+
+                        onShowAdCompleteListener.onShowAdComplete()
+                        loadAd(activity)
+                    }
+
+                    override fun onAdShowedFullScreenContent() {
+                        // Called when fullscreen content is shown.
+                        Log.d("NAGRAJ", "Ad showed fullscreen content.")
+                    }
+                })
+            isShowingAd = true
+            appOpenAd?.show(activity)
+        }
+
+        private fun wasLoadTimeLessThanNHoursAgo(numHours: Long): Boolean {
+            val dateDifference: Long = Date().time - loadTime
+            val numMilliSecondsPerHour: Long = 3600000
+            return dateDifference < numMilliSecondsPerHour * numHours
+        }
+
         private fun isAdAvailable(): Boolean {
-            return appOpenAd != null
+            return appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4)
         }
     }
 
@@ -96,5 +188,10 @@ class App : Application(), Application.ActivityLifecycleCallbacks {
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
 
     override fun onActivityDestroyed(activity: Activity) {}
+
+    /** Interface definition for a callback to be invoked when an app open ad is complete. */
+    interface OnShowAdCompleteListener {
+        fun onShowAdComplete()
+    }
 
 }
