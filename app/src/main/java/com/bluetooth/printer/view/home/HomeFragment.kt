4a6abdf5f6a289378dbef7e9ebe3c50a925bd010
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-07-23 20:17:07 +0530

    ads

--- app/src/main/java/com/bluetooth/printer/view/home/HomeFragment.kt
+++ app/src/main/java/com/bluetooth/printer/view/home/HomeFragment.kt
@@ -1,33 +1,18 @@
 package com.bluetooth.printer.view.home
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.bluetooth.printer.R
 import com.bluetooth.printer.databinding.FragmentHomeBinding
 import com.bluetooth.printer.view.base.BaseFragment
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 
 class HomeFragment : BaseFragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val viewModel: HomeViewModel by activityViewModels()
-    private var map: GoogleMap? = null
-    private var cl: LatLng? = null
-
 
     private val binding get() = _binding!!
 
@@ -35,51 +20,11 @@ class HomeFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        MapsInitializer.initialize(requireContext(), MapsInitializer.Renderer.LATEST) {}
         val root: View = binding.root
         binding.btnRefresh.setOnClickListener {
-            activity?.let { it1 ->
-                (activity as HomeActivity).updateLocation()
-            }
-        }
-
-
-        //Maps View
-        binding.mvCl.onCreate(savedInstanceState)
-        binding.mvCl.getMapAsync() {
-
-            val circleDrawable = resources.getDrawable(R.drawable.user, null)
-            val markerIcon: BitmapDescriptor = getMarkerIconFromDrawable(circleDrawable)
-            map = it
-
-
 
         }
         return root
-    }
-
-
-
-
-    override fun onStart() {
-        super.onStart()
-        binding.mvCl.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.mvCl.onResume()
-    }
-
-
-    override fun onStop() {
-        super.onStop()
-        binding.mvCl.onStop()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        binding.mvCl.onLowMemory()
     }
 
 
@@ -94,17 +39,6 @@ class HomeFragment : BaseFragment() {
 
     override fun hideProgress() {
         TODO("Not yet implemented")
-    }
-
-    private fun getMarkerIconFromDrawable(drawable: Drawable): BitmapDescriptor {
-        val canvas = Canvas()
-        val bitmap = Bitmap.createBitmap(
-            drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888
-        )
-        canvas.setBitmap(bitmap)
-        drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-        drawable.draw(canvas)
-        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
 
