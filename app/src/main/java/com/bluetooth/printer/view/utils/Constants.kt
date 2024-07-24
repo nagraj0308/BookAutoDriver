69f66de20bc2da634d1005ddb1541f7f9b708a17
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-07-24 07:08:03 +0530

    ads

--- app/src/main/java/com/bluetooth/printer/view/utils/Constants.kt
+++ app/src/main/java/com/bluetooth/printer/view/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.bluetooth.printer.view.utils
 class Constants {
     companion object {
         const val ABOUT_US_URL = "https://nagraj0308.github.io/btp_au/"
-        const val PNP_URL = "https://nagraj0308.github.io/btp_pnp/"
         const val PLAYSTORE_URL = "https://play.google.com/store/apps/details?id=com.bluetooth.printer"
     }
 }
