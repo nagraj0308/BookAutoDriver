506ce675955db470fbed745ae879a18c7af6f2fc
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-09-21 16:05:44 +0530

    ads

--- app/src/main/java/com/rent/house/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/rent/house/presentation/home/HomeActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.widget.Button
 import androidx.activity.result.ActivityResultLauncher
@@ -20,6 +21,11 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
@@ -49,6 +55,7 @@ import com.rent.house.utils.Constants
 import com.rent.house.utils.PermissionUtils
 import com.rent.house.utils.RequestCode
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -81,6 +88,20 @@ class HomeActivity : BaseActivity() {
         MapsInitializer.initialize(this, MapsInitializer.Renderer.LATEST) {}
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         headerBinding = NavHeaderMainBinding.bind(binding.navView.getHeaderView(0))
+
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@HomeActivity) {
+                val testDeviceIds = listOf("6C438043FBAE987EE0BA45F40294826C")
+                val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
+                MobileAds.setRequestConfiguration(configuration)
+
+                val adRequest: AdRequest =AdRequest.Builder().build()
+                val mAdView = findViewById<AdView>(R.id.banner_ad)
+                mAdView.loadAd(adRequest)
+            }
+        }
 
 
         setSupportActionBar(binding.appBarMain.toolbar)
@@ -134,7 +155,6 @@ class HomeActivity : BaseActivity() {
         }
         checkForAppUpdates()
         askNotificationPermission();
-
     }
 
 
