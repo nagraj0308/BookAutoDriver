a40eca3f979c78eedcac4171d210f25196f0dc46
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-10-13 13:22:50 +0530

    ads

--- app/src/main/java/com/bluetooth/printer/view/printpdf/PrintPDFActivity.kt
+++ app/src/main/java/com/bluetooth/printer/view/printpdf/PrintPDFActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothSocket
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import com.bluetooth.printer.PM
@@ -15,6 +16,7 @@ import com.bluetooth.printer.view.btdevice.BTDeviceListActivity
 import com.bluetooth.printer.view.utils.PermissionUtils
 import com.bluetooth.printer.view.utils.RequestCodeIntent
 import com.bluetooth.printer.view.utils.RequestCodePermission
+import com.bluetooth.printer.view.utils.Utils
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -30,9 +32,11 @@ class PrintPDFActivity : BaseActivity() {
     lateinit var pm: PM
 
     private var printType: PrintType? = null
-    private var printer: BluetoothSocket? = null
 
     private lateinit var binding: ActivityPrintPdfBinding
+
+
+    val ESC_ALIGN_CENTER: ByteArray = byteArrayOf(0x1b, 'a'.code.toByte(), 0x01)
 
 
     companion object {
@@ -60,12 +64,22 @@ class PrintPDFActivity : BaseActivity() {
             printer?.let { it1 ->
                 run {
                     if (it1.isConnected) {
-                        sendData(it1, " अंश नोबादा अंश नोबादा  अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा")
+                        Utils.screenShot(binding.ivSelectPdf)
+                            ?.let { it2 -> sendBitMapData(it1, it2) }
+//                        sendData(it1, " अंश नोबादा अंश नोबादा  अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा अंश नोबादा")
                     } else {
                         showToast("Printer not connected")
                         BTDeviceListActivity.start(this)
                     }
                 }
+            }
+        }
+        binding.tvFileName.text = "File Name : ";
+        binding.ivSelectPdf.setOnClickListener{
+            if(PermissionUtils.checkReadStoragePermission(this)){
+                Utils.openPDFChooser(this)
+            }else{
+                PermissionUtils.requestReadStoragePermission(this)
             }
         }
     }
@@ -74,13 +88,17 @@ class PrintPDFActivity : BaseActivity() {
         if (PermissionUtils.checkBTDeviceConnectPermission(this)) {
             if(BluetoothAdapter.checkBluetoothAddress(pm.btDeviceAddress)){
                 CoroutineScope(Dispatchers.IO).launch {
-                    printer = BluetoothConnection().connectToDevice(pm.btDeviceAddress)
-                    withContext(Dispatchers.Main) {
-                        if (printer == null || !printer!!.isConnected) {
-                            showToast("Failed to connect to printer")
-                        }else{
-                            showToast("Connected to printer")
+                    if (printer == null || !printer!!.isConnected) {
+                        printer = BluetoothConnection().connectToDevice(pm.btDeviceAddress)
+                        withContext(Dispatchers.Main) {
+                            if (printer == null || !printer!!.isConnected) {
+                                showToast("Failed to connect to printer")
+                            }else{
+                                showToast("Connected to printer")
+                            }
                         }
+                    }else{
+                        showToast("Connected to printer")
                     }
                 }
             }else{
@@ -103,6 +121,11 @@ class PrintPDFActivity : BaseActivity() {
         ) {
             connectBTDevice()
         }
+
+        if (requestCode == RequestCodePermission.READ_STORAGE && PermissionUtils.checkReadStoragePermission(this)
+        ) {
+            Utils.openPDFChooser(this)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -115,6 +138,17 @@ class PrintPDFActivity : BaseActivity() {
             val outputStream = socket.outputStream
             outputStream.write(data.toByteArray())
         } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun sendBitMapData(socket: BluetoothSocket, data: Bitmap) {
+        try {
+            val outputStream = socket.outputStream
+            val bytes: ByteArray = Utils.bitmapToBytes(data)
+            outputStream.write(ESC_ALIGN_CENTER)
+            outputStream.write(bytes)
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
@@ -141,7 +175,9 @@ class PrintPDFActivity : BaseActivity() {
         if (requestCode == RequestCodeIntent.READ_CONTENT && resultCode == Activity.RESULT_OK) {
             val pdfUri: Uri? = data?.data
             pdfUri?.let {
-                  ///
+                it.path?.let { it1 -> Utils.renderPdfPage(it1,1) ?.let { it2 ->
+                    printer?.let { it3 -> it2?.let { it4 -> sendBitMapData(it3, it4) } }
+                }}
              }
         }
     }
