da6f10998beda29c66153c33aad306ab30fd9d54
nagraj0308 <nagendrachaudhary2016@gmail.com>
2024-10-16 15:32:02 +0530

    ads

--- app/src/main/java/com/book/auto/utils/Utils.kt
+++ app/src/main/java/com/book/auto/utils/Utils.kt
@@ -1,7 +1,16 @@
 package com.book.auto.utils
 
 
+import android.content.Context
 import android.location.Location
+import android.util.Log
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
 class Utils {
@@ -10,6 +19,30 @@ class Utils {
             val results = FloatArray(1)
             Location.distanceBetween(lat1, lon1, lat2, lon2, results)
             return String.format("%.2f", results[0] / 1000).toDouble()
+        }
+
+        fun printAdvertisingId( context : Context){
+            CoroutineScope(Dispatchers.IO).launch {
+
+                var idInfo: AdvertisingIdClient.Info? = null
+                try {
+                    idInfo = AdvertisingIdClient.getAdvertisingIdInfo(context)
+                } catch (e: GooglePlayServicesNotAvailableException) {
+                    e.printStackTrace()
+                } catch (e: GooglePlayServicesRepairableException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                var advertId: String? = null
+                try {
+                    advertId = idInfo!!.id
+                } catch (e: NullPointerException) {
+                    e.printStackTrace()
+                }
+                Log.d("NAGRAJ", "$advertId")
+
+            }
         }
     }
 }
